5e81024d4e5b2ac76f594497f2cda358fb0aa46a
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-14 18:26:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4].insert(element, array)
+
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
